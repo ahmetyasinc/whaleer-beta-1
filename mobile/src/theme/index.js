62eccae82fe97af0892f6157371914f7c0631aa5
megasolo25 <megasolo25@gmail.com>
2026-02-01 17:22:52 +0300

    mobile geçici olarak kaldırıldı

--- mobile/src/theme/index.js
+++
@@ -1,7 +0,0 @@
-// src/theme/index.js
-import light from "./light";
-import dark from "./dark";
-
-export const getTheme = (mode) => {
-  return mode === "dark" ? dark : light;
-};
--- mobile/src/theme/spacing.js
+++
@@ -1,8 +0,0 @@
-// src/theme/spacing.js
-export default {
-  xs: 4,
-  sm: 8,
-  md: 16,
-  lg: 24,
-  xl: 32,
-};
